3f3a15235b91b5fb99c623c40a54a7f2d16321c8
Yana3558 <ya.pana3558@gmail.com>
2022-01-24 10:30:36 +0300

    lesson 1 added file

--- YaPan_les1.py
+++ YaPan_les1.py
@@ -1,3 +1,17 @@
+import requests
+import json
+from pprint import pprint
 
+username = 'YanaPan'
+url = f'https://api.github.com/users/{username}/repos'
 
-print("Hello")
+response = requests.get(url)
+j_data = response.json()
+# pprint(type(response.text))
+# response.content
+# response.text
+
+pprint(type(j_data))
+
+# with open('data.txt', 'w') as outfile:
+#     json.dump(data, outfile)
